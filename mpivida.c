920a311041b9be6eb966c95c1a52a9961442e3cc
jrzanol <jrzanol.92@gmail.com>
2024-03-01 14:53:01 -0300

    final version.

--- mpivida.c
+++ mpivida.c
@@ -1,16 +1,75 @@
+// Nome: Ronei Angelo Zanol Junior.
+// Turma: IPPD-202302
+
 #include <mpi.h>
 #include <time.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <assert.h>
 
 #define N 8
 unsigned char g_Game[N][N];
 
+void Show(int i)
+{
+    // Exibe o tabuleiro do jogo:
+    printf("Jogo da Vida #%02d:\n", i);
+
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < N; ++x)
+            printf("%d ", g_Game[y][x]);
+
+        printf("\n");
+    }
+}
+
+int GetCount(int _x, int _y)
+{
+    int count = 0;
+
+    for (int y = _y - 1; y <= (_y + 1); ++y)
+    {
+        for (int x = _x - 1; x <= (_x + 1); ++x)
+        {
+            if (x < 0 || x >= N || y < 0 || y >= N)
+                continue; // Limite do tabuleiro.
+
+            if (g_Game[y][x] == 1)
+                count++;
+        }
+    }
+
+    return count;
+}
+
 void Process(int rank, int baseXProcess, int baseYProcess, int numPerProcess)
 {
+    int y = baseYProcess;
+    int x = baseXProcess;
+
+    for (int num = 0; num < numPerProcess; ++num)
+    {
+        int count = GetCount(x, y);
+
+        if (g_Game[y][x] == 0)
+        {
+            if (count == 3)
+                g_Game[y][x] = 1;
+        }
+        else
+        {
+            if (count < 2 || count > 3)
+                g_Game[y][x] = 0;
+        }
+
+        if (++x >= N)
+        {
+            x = 0;
+            y++;
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -33,13 +92,15 @@ int main(int argc, char *argv[]) {
     {
         memset(g_Game, 0, sizeof(g_Game));
 
-        int min = (N / 10);
+        int min = (N / 3);
         if (min < 1)
             min = 1;
 
         for (int i = 0; i < N; ++i)
             for (int j = 0; j < min; ++j)
                 g_Game[i][rand() % N] = 1;
+
+        Show(-1);
 
         for (int i = 0; i < 10; ++i)
         {
@@ -64,16 +125,7 @@ int main(int argc, char *argv[]) {
                     printf("Process(%d/%d): MPI_Recv fail.\n", rank, size);
             }
 
-            // Exibe o tabuleiro do jogo:
-            printf("Jogo da Vida #%02d:\n", i);
-
-            for (int y = 0; y < N; ++y)
-            {
-                for (int x = 0; x < N; ++x)
-                    printf("%d ", g_Game[y][x]);
-
-                printf("\n");
-            }
+            Show(i);
         }
     }
     else
